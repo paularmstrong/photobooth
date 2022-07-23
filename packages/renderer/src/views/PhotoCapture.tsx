2c6b890c2eed40c29c8968ba75003a9c5dea8f6f
Paul Armstrong <paul@spaceyak.com>
2022-07-23 10:48:46 -0700

    feat: show photo after capture

--- packages/renderer/src/views/PhotoCapture.tsx
+++ packages/renderer/src/views/PhotoCapture.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import clsx from 'clsx';
+import { CSSTransition } from 'react-transition-group';
 import { useMediaStream, usePhotos } from '../context';
 import { Card, CountdownCircle, Shutter } from '../components';
 import cameraShutterSound from '../sounds/camera-shutter.mp3';
@@ -7,14 +9,26 @@ import toneSound from '../sounds/tone.wav';
 export function PhotoCapture() {
   const [taking, setTaking] = React.useState(false);
   const { addPhoto, photos } = usePhotos();
+  const [showLastPhoto, setShowLastPhoto] = React.useState(false);
   const mediaStream = useMediaStream();
+  const canvas = React.useRef<HTMLCanvasElement>(null);
 
   const shutter = React.useMemo(() => new Audio(cameraShutterSound), []);
   const tone = React.useMemo(() => new Audio(toneSound), []);
 
   React.useEffect(() => {
-    if (photos.length >= 4) {
-      window.api.send('transition', { type: 'DONE' });
+    if (photos.length && canvas.current) {
+      const ctx = canvas.current.getContext('2d')!;
+      const { width, height } = canvas.current;
+      const photo = photos[photos.length - 1];
+      setShowLastPhoto(true);
+      ctx.drawImage(photo, 0, 0, photo.width, photo.height, 0, 0, width, height);
+      setTimeout(() => {
+        setShowLastPhoto(false);
+        if (photos.length >= 4) {
+          window.api.send('transition', { type: 'DONE' });
+        }
+      }, 1500);
     }
   }, [photos]);
 
@@ -41,9 +55,9 @@ export function PhotoCapture() {
                 const imageCapture = new ImageCapture(track);
                 const start = Date.now();
                 imageCapture.grabFrame().then((bitmap) => {
+                  addPhoto(bitmap);
                   setTimeout(() => {
                     setTaking(false);
-                    addPhoto(bitmap);
                   }, Math.min(500, 500 - (Date.now() - start)));
                 });
               }}
@@ -51,7 +65,26 @@ export function PhotoCapture() {
           ) : null}
         </Card>
       </div>
+
+      <div className="absolute inset-0 flex flex-row justify-center items-center">
+        <CSSTransition in={showLastPhoto} classNames={transitionClassnames} timeout={300}>
+          <canvas ref={canvas} className={clsx('w-1/2 aspect-[16/10]', { 'opacity-0': !showLastPhoto })} />
+        </CSSTransition>
+      </div>
+
       {taking ? <Shutter /> : null}
     </>
   );
 }
+
+const allClasses = 'p-3 bg-white drop-shadow-2xl transition-all ease-in-out duration-150 delay-150';
+const startClasses = 'scale-90 -rotate-6 opacity-0';
+const showClasses = 'scale-100 rotate-6 opacity-1';
+const transitionClassnames = {
+  enter: clsx(allClasses, startClasses),
+  enterActive: clsx(allClasses, startClasses),
+  enterDone: clsx(allClasses, showClasses),
+  exit: clsx(allClasses, showClasses),
+  exitActive: clsx(allClasses, showClasses),
+  exitDone: clsx(allClasses, startClasses),
+};
